a857e35fdedaf21c49ff6c1b348b79e1552ea7e7
pablohirafuji <pablohirafuji@gmail.com>
2019-12-01 23:43:44 +0000

    Deploying to gh-pages from master fd185f7b83ee52228241c0a733f6987e03d07a67

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62c364fd4691138b38310f66e2ff723d.js"
+  "elm-qrcode/precache-manifest.b87eed52b4409502b762255e0cb70495.js"
 );
 
 workbox.core.skipWaiting();
